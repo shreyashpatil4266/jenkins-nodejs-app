4ced7052b3fbbcabb5302305f3187100ecc93b4d
shreyashpatil4266 <k46shreyashpatil@gmail.com>
2025-04-08 21:48:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const http = require('http');
 
 const port = 3000;
 const server = http.createServer((req, res) => {
-  res.end(' Hello from Jenkins CI/CD app!');
+  console.log("✨ App has started with latest changes!");
+  res.end('✅ CI/CD update: clean rebuild');
 });
 
 server.listen(port, () => {
   console.log(`🚀 Server is running on http://localhost:${port}`);
 });
-
